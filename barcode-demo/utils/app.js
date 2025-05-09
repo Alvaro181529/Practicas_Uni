620766e33da84615221efe03c9f76f400a0ccf06
alvaro medrano <aljak1815@gmail.com>
2025-05-08 22:26:30 -0400

    code bar

---
+++ barcode-demo/utils/app.js
@@ -0,0 +1,30 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err));
+  }
+  
+  let deferredPrompt;
+  
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Evitar que el navegador muestre automáticamente el cuadro de diálogo
+    e.preventDefault();
+    deferredPrompt = e;
+    
+    // Mostrar el botón para permitir la instalación
+    const installButton = document.getElementById('install-button');
+    installButton.style.display = 'block';
+  
+    // Manejar el clic en el botón de instalación
+    installButton.addEventListener('click', () => {
+      // Mostrar el cuadro de instalación
+      deferredPrompt.prompt();
+  
+      // Esperar a que el usuario elija
+      deferredPrompt.userChoice.then((choiceResult) => {
+        console.log(choiceResult.outcome);
+        deferredPrompt = null;
+      });
+    });
+  });
+  
